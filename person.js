46cf4d0a4161fdc96834b0270191bcd5d2cf83ac
root <root@localhost.localdomain>
2015-06-13 23:12:48 -0700

    messing around with how a person associates with their environment and peers

---
+++ person.js
@@ -0,0 +1,42 @@
+function Person(race, sex, environment, orientation, peers) {
+    this.race = race;
+    this.sex = sex;
+    this.orientation = orientation;
+    this.associateWith = this.develoup(race, sex, environment, peers);
+}
+
+Person.prototype.develoup = function(race, sex, environment, peers) {
+    if(this.orientation === 'cis') {
+        return this.raceCheck(race, sex, environment, peers);
+    } else {
+        if(this.sex === 'female') {
+            sex = 'male';
+            return this.raceCheck(race, sex, environment, peers);
+        } else {
+            sex = 'female';
+            return this.raceCheck(race, sex, environment, peers);
+        }
+    }
+};
+
+Person.prototype.raceCheck = function(race, sex, environment, peers) {
+    if(peers.race === this.race) {
+        return this.combine(race, sex, environment);
+    } else {
+        race = peers.race;
+        return this.combine(race, sex, environment);
+    }
+};
+
+Person.prototype.combine = function(race, sex, environment) {
+    return race + ' ' + sex + ' ' + environment;
+};
+
+var bobFriend = {
+    race: "white",
+    sex: "female",
+    environment: "middleclass",
+    orientation:  "cis" 
+};
+var bob = new Person("african", "male", "middleclass", "trans", bobFriend);
+bob.associateWith;
